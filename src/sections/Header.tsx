33600b865f2e6fc96fafe718e45b0c55e57fde7c
MikiZenebe <mikizenebe10gmail.com>
2024-09-10 00:31:20 +0300

    Hero

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
           Streamline your workflow and boost your productivity
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,34 +1,58 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImg from "@/assets/cog.png";
+import cylinderImg from "@/assets/cylinder.png";
+import noddleImg from "@/assets/noodle.png";
 import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20">
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#eaeefe_100%)] overflow-x-clip">
       <div className="container">
-        <div>
-          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
-            Version 2.0 is here
-          </div>
-          <h1 className="text-5xl font-bold bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
-            Pathway to productivity
-          </h1>
-          <p className="text-xl text-[#010d3e] mt-6">
-            Celebrate the joy of accomplishment with an app designed to track
-            your progress, motivate your efforts, and celebrate your successes.
-          </p>
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="text-5xl md:text-7xl font-bold bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
+              Pathway to productivity
+            </h1>
+            <p className="text-xl text-[#010d3e] mt-6">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivate your efforts, and celebrate your
+              successes.
+            </p>
 
-          <div className="flex gap-1 items-center mt-[30px]">
-            <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text gap-1">
-              <span>Learn more</span>
-              <ArrowIcon className="h-5 w-5" />
-            </button>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
           </div>
-        </div>
 
-        <div className="mt-20">
-          <Image src={cogImg} alt="cog-image" />
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={cogImg}
+              alt="cog-image"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            />
+
+            <Image
+              src={cylinderImg}
+              width={220}
+              height={220}
+              alt="cyl-image"
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            />
+
+            <Image
+              src={noddleImg}
+              width={220}
+              alt="nod-image"
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
+          </div>
         </div>
       </div>
     </section>
